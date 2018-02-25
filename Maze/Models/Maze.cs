cf74788b61d21412469aeb039400a37cf22912e4
Justas Miknys <jmiknys@scottlogic.co.uk>
2018-02-25 14:04:37 +0000

    add entity framework configuration

--- Maze/Models/Maze.cs
+++ Maze/Models/Maze.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Maze.Models
 {
 	public class Maze
 	{
+		public int Id { get; set; }
+
+		[Required, StringLength(25)]
 		public string Name { get; set; }
+
 		public MazeConfiguration Configuration { get; set; }
 	}
 }
---
+++ Maze/Models/MazeDbConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Maze.Models
+{
+	public class MazeDbConfiguration : IEntityTypeConfiguration<Maze>
+	{
+		public void Configure(EntityTypeBuilder<Maze> builder)
+		{
+			builder.HasKey(maze => maze.Id);
+		}
+	}
+}
